362951c124c42af58bce78db1417cea46f783d1c
MateoLopezSena <mateolopez2310@gmail.com>
2022-11-28 18:54:45 -0500

    Crear header

---
+++ header.php
@@ -0,0 +1,17 @@
+<header>
+    <div>
+        <h1> NOMBRE DE LA PAGINA </h1>
+    </div>
+    <nav>
+        <ul class="links">
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Deporte</a></li>
+            <li><a href="">Deporte</a></li>
+            <li><a href="">Deporte</a></li>
+        </ul>
+    </nav>
+    <div class="user">
+        <a href="" class="cart fa-sharp fa-solid fa-cart-shopping"></a>
+        <a href="" class="login fa-solid fa-user" ></a>
+    </div>
+</header>

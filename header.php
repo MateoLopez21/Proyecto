816121cbad63724d08877d824b00063d8cfd188d
MateoLopez21 <mateolopez972@gmail.com>
2022-11-28 20:05:33 -0500

    Crear Header

---
+++ header.php
@@ -0,0 +1,17 @@
+<header>
+    <div>
+        <h1> NOMBRE DE LA PAGINA </h1>
+    </div>
+    <nav>
+        <ul class="links">
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="futbol.php">Fútbol</a></li>
+            <li><a href="tenis.php">Tenis</a></li>
+            <li><a href="baloncesto.php">Baloncesto</a></li>
+        </ul>
+    </nav>
+    <div class="user">
+        <a href="favoritos.php" class="cart fa-sharp fa-solid fa-heart"></a>
+        <a href="login.php" class="login fa-solid fa-user" ></a>
+    </div>
+</header>

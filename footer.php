4e2d9615c7f5706b74b080ac00dcbbf0978586b1
YesicaGiraldo04 <yesicagiraldo2004@gmail.com>
2022-11-29 20:20:33 -0500

    creacion login

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div>
+        <h3>Contactenos: </h3>
+    </div>
+    <div>
+        <p>
+            Celular: 3014857291 <br>
+            Correo: fittedly@gmail.com
+        </p>
+    </div>
+</footer>
--- pagina/iniciarSesion.php
+++ pagina/iniciarSesion.php
@@ -19,15 +19,18 @@
 </head>
 <body>
     <?php require './header.php' ?>
+
     <div class="container">
-        <form action="" method="post" class=" mx-auto d-block text-center">
-            <label for="" class="form-label">Correo Electronico</label> <br>
-            <input type="text" class="form-control"> <br>
-            <label for="" class="form-label">Contraseña</label> <br>
-            <input type="password" class="form-control"> <br>
-            <input type="submit" value="Iniciar Sesión" class="btn btn-primary" >
-            <input type="button" value="Crear Cuenta" class="btn btn-primary" onclick="window.location='./registrarse.php'">
-        </form>
+        <div method="POST" class="formulario">
+            <form action="" method="post" class=" mx-auto d-block text-center">
+                <label for="" class="form-label">Correo Electronico</label> <br>
+                <input type="text" class="form-control"> <br>
+                <label for="" class="form-label">Contraseña</label> <br>
+                <input type="password" class="form-control"> <br>
+                <input type="submit" value="Iniciar Sesión" class="btn btn-primary" >
+                <input type="button" value="Crear Cuenta" class="btn btn-primary" onclick="window.location='./registrarse.php'">
+            </form>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" 
     crossorigin="anonymous"></script>

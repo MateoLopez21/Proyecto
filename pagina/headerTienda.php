41f57bd336b63466605526625948723a87a33b5d
MateoLopez21 <mateolopez972@gmail.com>
2022-12-01 18:45:21 -0500

    headerTienda creado

---
+++ pagina/headerTienda.php
@@ -0,0 +1,17 @@
+<header>
+    <div>
+        <a href="./index.php"><h1> SportSize </h1></a>
+    </div>
+    <nav>
+        <ul class="links">
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="#futbol">Fútbol</a></li>
+            <li><a href="#tenis">Tenis</a></li>
+            <li><a href="#balonesto">Baloncesto</a></li>
+        </ul>
+    </nav>
+    <div class="user">
+        <a href="favoritos.php" class="cart fa-sharp fa-solid fa-heart"></a>
+        <a href="login.php" class="login fa-solid fa-user" ></a>
+    </div>
+</header>

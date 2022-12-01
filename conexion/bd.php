5f05178ec2992294d99056428fb7a2577fbadd38
MateoLopez21 <mateolopez972@gmail.com>
2022-11-30 21:43:19 -0500

    Actualizar conexion

--- conexion/bd.php
+++ conexion/bd.php
@@ -1,26 +1,41 @@
 <?php
-    class DB {
-        protected static $pdo;
-        private function __construct(){
-            try{
-                self::$pdo = new PDO(    
-                    'mysql:host=localhost;dbname=tiendadeportiva','root','');
-                    self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                    self::$pdo->setAttribute (PDO::ATTR_PERSISTENT, false);
-            } catch (PDOException $e){
-                echo "No hemos podido conectar con a base de datos.";
-                exit;
-            }
-        }
-        public static function getConn(){
-            if(!self::$pdo){
-                new DB();
-            }
-            return self::$pdo;
+    $server = 'localhost';
 
-        }
+    $userName = 'root';
 
+    $password = '';
+
+    $dataBase = 'tiendadeportiva';
+
+    try {
+        $conn = new PDO("mysql:host=$server;dbname=$dataBase;",$userName, $password);
+    } catch (PDOException $error){
+        die('Connection failed: '.$error->getMessage());
     }
+
+
+    // class DB {
+    //     protected static $pdo;
+    //     private function __construct(){
+    //         try{
+    //             self::$pdo = new PDO(    
+    //                 'mysql:host=localhost;dbname=tiendadeportiva','root','');
+    //                 self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //                 self::$pdo->setAttribute (PDO::ATTR_PERSISTENT, false);
+    //         } catch (PDOException $e){
+    //             echo "No hemos podido conectar con a base de datos.";
+    //             exit;
+    //         }
+    //     }
+    //     public static function getConn(){
+    //         if(!self::$pdo){
+    //             new DB();
+    //         }
+    //         return self::$pdo;
+
+    //     }
+
+    // }
 
 
 ?>

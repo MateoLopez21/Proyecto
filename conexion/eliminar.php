02e5600a04b23cfc45f6963e728baeadbc33fa2a
MateoLopez21 <mateolopez972@gmail.com>
2022-12-01 21:28:48 -0500

    Eliminar creado

---
+++ conexion/eliminar.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+require '../conexion/bd.php';
+
+if(isset($_POST['eliminar'])){
+    $sql = "DELETE FROM usuario WHERE idUsuario = :id";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam('id', $_SESSION['user_id']);
+
+    if($stmt->execute()){
+        echo "<script>
+        alert('Usuario eliminado con éxito.')
+        window.location.href='../pagina/index.php';
+        </script>";
+    }else{
+        echo "<script>
+        alert('Error al eliminar el usuario.')
+        window.location.href='../pagina/index.php';
+        </script>";
+    }
+}
